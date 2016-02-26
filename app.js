2b41b2ebcb0d99f6c431e49c614916cae26e9fa3
Gunther Schneider <pixeloon@gmail.com>
2016-02-25 22:41:59 -0800

    completed main exercises

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 console.log("Javascript is alive!");
 var images = document.querySelectorAll("img");
+var ghost = document.getElementById("ghosting");
+var size = document.getElementById("resize");
+var button = document.getElementById("reset");
 
 // Change the greeting from "Hello, There!" to "Hello, World!".
 
@@ -28,6 +31,7 @@ function listSelect() {
   } else {
     this.setAttribute("class","selected");
   }
+  // Change the image to be the most recently clicked food item.
   images[0].setAttribute("src","./images/" + this.innerText + ".jpeg");
 }
 
@@ -35,15 +39,49 @@ for (var i = 0; i < list.length; i++) {
   list[i].addEventListener('click', listSelect);
   } 
 
-
-// Change the image to be the most recently clicked food item.
-
-
-
 // When the gray div is moused over, it's removed from the DOM.
 
+ghost.addEventListener('mouseover', ghostify);
+
+function ghostify() {
+  ghost.parentNode.removeChild(ghost);
+}
+
 // When the orange div is moused over, its width doubles. When the mouse moves out of the div, it returns to its original size.
+size.addEventListener('mouseover', upsize);
+size.addEventListener('mouseleave', downsize);
+
+function downsize() {
+    size.removeAttribute("style");
+}
+
+function upsize() {
+  size.setAttribute("style", "width:400px");
+}
 
 // When the reset button is clicked - remove the selected class from each <li> and change the image to panic.jpeg.
+reset.addEventListener('click', panic);
+
+function panic(){
+  for (var i = 0; i < list.length; i++) {
+    list[i].removeAttribute("class");
+  }
+  images[0].setAttribute("src","./images/panic.jpeg");
+}
 
 // When the 1, 2, 3, 4, 5, 6, 7, 8, 9, or 0 key is pressed, the page alerts the message "I HATE NUMBERZZZ!"
+
+document.addEventListener("keydown", keyhandler, false);
+
+function keyhandler(event) {
+var keyCode = event.keyCode;
+  if ((keyCode===48) || (keyCode===49) || (keyCode===50) || (keyCode===51) || (keyCode===52) || (keyCode===53) || (keyCode===54) || (keyCode===55) || (keyCode===56) || (keyCode===57)) {
+  alert("I HATE NUMBERZZZ");
+  } 
+}
+
+
+
+
+
+
